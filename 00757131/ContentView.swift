45e5f45b8ebfeb8f880e8b5b40e29c2d04e6ac28
Wilhelm Shen <33245687+WilhelmIllust@users.noreply.github.com>
2020-09-30 10:29:44 +0800

    Initial Commit

---
+++ 00757131/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  00757131
+//
+//  Created by User11 on 2020/9/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 00757131/_0757131App.swift
@@ -0,0 +1,17 @@
+//
+//  _0757131App.swift
+//  00757131
+//
+//  Created by User11 on 2020/9/30.
+//
+
+import SwiftUI
+
+@main
+struct _0757131App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
